e2bedbc03beb0899d5ee4565dc9e9e445865d1a6
lainecedes <elaine.parieh@gmail.com>
2025-02-17 10:59:06 +0100

    Initialize project

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>This is the about page!</h1>
+        <br>
+        <NuxtLink to="/">Home</NuxtLink>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>This is the home page!</h1>
+        <br>
+        <NuxtLink to="/about">About</NuxtLink>
+    </div>
+</template>
